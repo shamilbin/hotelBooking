6b221de4fe9c4d6346b711435de85290f21fdce4
shamilbin <shamiltk029@gmail.com>
2025-10-13 16:22:18 +0530

    hero done clerk installed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
