2ecb1c557b69a7ea1bb329d890396471d1f9b915
shamilbin <shamiltk029@gmail.com>
2025-10-31 03:19:21 +0530

    for vervel

--- server/configs/dbConnect.js
+++ server/configs/dbConnect.js
@@ -5,6 +5,7 @@ dotenv.config()
 const dbConnect= async ()=>{
 
     try {
+        
         // mongoose.connection.on(`connected` ,()=>console.log(`Data Base Connected Succesfully`))
         await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`)
         console.log(`Data base Connected Succesfully`)
